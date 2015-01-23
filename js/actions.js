6c81be4e04c0f5bc46b04dd656596b356cb8ed01
Jerissa Lumansoc <jerissalumansoc@gmail.com>
2015-01-23 13:30:21 -0800

    local navigation working

--- js/actions.js
+++ js/actions.js
@@ -30,10 +30,52 @@ function AboutLocalNav() {
 	$(".content").hide();
 	$(".default-content").show();
 
-	$(".local-nav li").click(function() {
+	// $(".local-nav li").click(function() {
+	// 	$(".content").hide();
+	// 	if($(this).hasClass("local-nav-active")) {
+	// 		$(this).removeClass("local-nav-active");
+	// 	}
+	// 	$(this).find("span").removeClass("local-nav-arrow");
+	// });
+
+
+	$(".philosophy").click(function() {
 		$(".content").hide();
 		$(".local-nav li").removeClass("local-nav-active");
-		$(this + " span").addClass("local-nav-arrow");
-		$(this).addClass("local-nav-active");
+		$(".philosophy").addClass("local-nav-active");
+		
+		$(".local-nav li span").html("&nbsp;&nbsp;");
+		$(".philosophy span").html("<");
+		$(".philosophy-content").show();
+	});
+
+	$(".curriculum").click(function() {
+		$(".content").hide();
+		$(".local-nav li").removeClass("local-nav-active");
+		$(".curriculum").addClass("local-nav-active");
+		
+		$(".local-nav li span").html("&nbsp;&nbsp;");
+		$(".curriculum span").html("<");
+		$(".curriculum-content").show();
+	});
+
+	$(".requirement").click(function() {
+		$(".content").hide();
+		$(".local-nav li").removeClass("local-nav-active");
+		$(".requirement").addClass("local-nav-active");
+		
+		$(".local-nav li span").html("&nbsp;&nbsp;");
+		$(".requirement span").html("<");
+		$(".requirement-content").show();
+	});
+
+	$(".cost").click(function() {
+		$(".content").hide();
+		$(".local-nav li").removeClass("local-nav-active");
+		$(".cost").addClass("local-nav-active");
+		
+		$(".local-nav li span").html("&nbsp;&nbsp;");
+		$(".cost span").html("<");
+		$(".cost-content").show();
 	});
 }
